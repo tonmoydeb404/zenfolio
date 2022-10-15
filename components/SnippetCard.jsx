8aee46425a713555951176538c99a75e8bf3d2ab
tonmoydeb404 <tonmoydeb404@gmail.com>
2022-10-15 18:40:15 +0600

    snippets infinite scroll added

--- components/SnippetCard.jsx
+++ components/SnippetCard.jsx
@@ -2,6 +2,24 @@ import Link from "next/link";
 import React from "react";
 import { Card } from "react-daisyui";
 
+export const SnippetCardSkeleton = () => {
+  return (
+    <div className="card snippet_card  bg-white dark:bg-slate-800 shadow-sm bordered  animate-pulse">
+      <figure>
+        <div className="p-5 bg-slate-200 dark:bg-slate-700 rounded-full"></div>
+      </figure>
+      <div className="card-body">
+        <div className="h-3 bg-slate-200 dark:bg-slate-700 rounded mb-3"></div>
+        <div className="grid grid-cols-3 gap-x-4 gap-y-2 mb-5">
+          <div className="h-2 bg-slate-200 dark:bg-slate-700 rounded col-span-2"></div>
+          <div className="h-2 bg-slate-200 dark:bg-slate-700 rounded col-span-1"></div>
+          <div className="h-2 bg-slate-200 dark:bg-slate-700 rounded col-span-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const SnippetCard = ({
   icon = null,
   title = null,
--- pages/snippets/index.js
+++ pages/snippets/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import FetchErrorHandler from "../../components/FetchErrorHandler";
 import Header from "../../components/Header";
 import SEOHead from "../../components/SEOHead";
-import SnippetCard from "../../components/SnippetCard";
+import SnippetCard, { SnippetCardSkeleton } from "../../components/SnippetCard";
 import { getSnippetData } from "../../services";
 
 export const getStaticProps = async () => {
@@ -17,6 +18,38 @@ export const getStaticProps = async () => {
 };
 
 const Snippets = ({ data, error }) => {
+  // all snippets
+  const [allData, setAllData] = useState([]);
+
+  // pagination states
+  const [currentData, setCurrentData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataPerPage] = useState(5);
+  const hasMore = allData.length != currentData.length;
+
+  // update and filter all snippets from data
+  useEffect(() => {
+    if (data && data?.length) {
+      // clone snippets
+      let snippets = [...data];
+
+      // update snippets
+      setAllData([...snippets]);
+    }
+  }, [data]);
+
+  // update current snippets for pagination
+  useEffect(() => {
+    if (allData && allData.length) {
+      setCurrentData([...allData.slice(0, dataPerPage * currentPage)]);
+    }
+  }, [allData, dataPerPage, currentPage]);
+
+  // handle load more fuction for pagination
+  const handleLoadMore = () => {
+    setCurrentPage((prevValue) => prevValue + 1);
+  };
+
   return (
     <>
       <SEOHead title={"Snippets - Tonmoy Deb"} path="/Snippets" />
@@ -30,20 +63,31 @@ const Snippets = ({ data, error }) => {
       />
 
       <FetchErrorHandler error={error} className="error_msg-1">
-        {/* snippet content area */}
-        <div className="snippets_content">
-          {data && data.length
-            ? data.map((snippet) => (
-                <SnippetCard
-                  key={snippet.id}
-                  title={snippet.title}
-                  subtitle={snippet.description}
-                  icon={snippet.icon}
-                  link={`/snippets/${snippet.slug}`}
-                />
-              ))
-            : "no snippets found"}
-        </div>
+        {allData && allData.length ? (
+          <InfiniteScroll
+            dataLength={currentData.length}
+            next={handleLoadMore}
+            hasMore={hasMore}
+            loader={<SnippetCardSkeleton />}
+            className="snippets_content"
+            scrollThreshold={0.9}
+            endMessage={
+              <p className="error_msg error_msg-3 mt-5">thats all for today</p>
+            }
+          >
+            {currentData.map((snippet) => (
+              <SnippetCard
+                key={snippet.id}
+                title={snippet.title}
+                subtitle={snippet.description}
+                icon={snippet.icon}
+                link={`/snippets/${snippet.slug}`}
+              />
+            ))}
+          </InfiniteScroll>
+        ) : (
+          <SnippetCardSkeleton />
+        )}
       </FetchErrorHandler>
     </>
   );
