d19aa4863d8a76c009d46460f5a28a89d686a0df
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-01-05 22:31:17 +0600

    footer updated

--- components/FooterSection.jsx
+++ components/FooterSection.jsx
@@ -39,23 +39,25 @@ const FooterSection = () => {
           </div>
         ) : null}
 
-        {DB.legalLinks && DB.legalLinks?.length ? (
-          <div>
-            <Footer.Title>Legal</Footer.Title>
-            {DB.legalLinks.map((item) => (
-              <Link
-                href={item.path}
-                key={item.id}
-                target={item.newTab ? "_blank" : "_self"}
-                className="link link-hover"
-              >
-                {item.title}
-              </Link>
-            ))}
-          </div>
-        ) : null}
+        <div>
+          {DB.legalLinks && DB.legalLinks?.length ? (
+            <>
+              <Footer.Title>Legal</Footer.Title>
+              {DB.legalLinks.map((item) => (
+                <Link
+                  href={item.path}
+                  key={item.id}
+                  target={item.newTab ? "_blank" : "_self"}
+                  className="link link-hover"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </>
+          ) : null}
+        </div>
       </Footer>
-      <div className="copyright py-3 px-2 text-sm text-center border-t border-t-gray-300">
+      <div className="copyright py-3 px-2 text-sm text-center border-t border-slate-700">
         &copy; all rights are reserved by{" "}
         <Link href={"/"} className="text-primary font-semibold">
           {DB.name.toLowerCase()}
