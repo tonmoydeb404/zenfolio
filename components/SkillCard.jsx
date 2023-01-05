ee091e24e0ce5a4a573445c8a4d0842e55bd6c1e
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-01-05 23:00:03 +0600

    skill card updated

--- components/SkillCard.jsx
+++ components/SkillCard.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-
-const progressLevel = {
-  beginner: 25,
-  junior: 50,
-  intermediate: 75,
-  expert: 100,
-};
+import skill from "../constants/skill.json";
 
 const SkillCard = ({ level = null, title = null }) => {
   return (
     <div
       className="skills_card"
-      style={{ "--progress": `${progressLevel[level.toLowerCase()] || 0}%` }}
+      style={{ "--progress": `${skill[level.toLowerCase()] || 0}%` }}
     >
       <h3 className="skills_title">{title}</h3>
       <span className="skills_level">{level.toLowerCase()}</span>
