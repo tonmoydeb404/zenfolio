081bd601e371c019abcb1ec5fc10ad57d49e46c9
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-06-25 02:06:40 +0600

    skill section updated

--- src/components/pages/home/HomeSkills.tsx
+++ src/components/pages/home/HomeSkills.tsx
@@ -1,4 +1,6 @@
 import SkillCard from "@/components/cards/SkillCard";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
 
 const HomeSkills = () => {
   return (
@@ -8,6 +10,15 @@ const HomeSkills = () => {
         Here I mentioned those technologies that I love to work with.
       </p>
 
+      <div className="grid sm:grid-cols-2 gap-3">
+        <SkillCard title="HTML" level="expert" />
+        <SkillCard title="CSS" level="expert" />
+      </div>
+      <div className="flex items-center py-8">
+        <Separator className="flex-1" />
+        <Badge className="text-sm rounded-sm">OTHER SKILLS</Badge>
+        <Separator className="flex-1" />
+      </div>
       <div className="grid sm:grid-cols-2 gap-3">
         <SkillCard title="HTML" level="expert" />
         <SkillCard title="CSS" level="expert" />
