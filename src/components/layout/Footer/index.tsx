c889335846d2d0f3d8eeb2e13c6f3894e35e8b2d
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-06-25 23:40:20 +0600

    footer section updated

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <Separator />
 
         <div className="flex flex-col sm:flex-row-reverse flex-wrap gap-5 items-center justify-center sm:justify-between px-3 py-4">
-          <ul className="icon-list gap-1">
+          <ul className="icon-list gap-0">
             <Link href={"#"} className="icon-list_icon text-2xl">
               <SiFacebook />
             </Link>
@@ -21,7 +21,7 @@ const Footer = () => {
               <SiGithub />
             </Link>
           </ul>
-          <div className="flex items-center">
+          <div className="flex items-center gap-1">
             <BiCopyright className="text-lg text-primary" />
             <p>all rights are reserved</p>
           </div>
